5a6ea8adbe6b43598c20513c56eb02ad13948e61
Vladissluff <165704824+Vladissluff@users.noreply.github.com>
2025-03-30 21:13:36 +0300

    goit-js-hw-12

---
+++ src/js/pixabay-api.js
@@ -0,0 +1,24 @@
+import axios from "axios";
+
+const API_KEY = `49390436-eaa1c4fe3003ec0e1553f6322`;
+const BASE_URL = `https://pixabay.com/api/`;
+
+export async function fetchImages(query, limit = 15, page = 1) {
+  try {
+    const response = await axios.get(BASE_URL, {
+      params: {
+        key: API_KEY,
+        q: query,
+        image_type: `photo`,
+        orientation: `horizontal`,
+        safesearch: true,
+        per_page: limit,
+        page: page,
+      }
+    });
+    return response.data;
+  } catch (error) {
+    console.error(`Error fetching images:`, error);
+    throw error;
+  }
+}
